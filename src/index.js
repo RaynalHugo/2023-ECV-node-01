cf1505fc4ad5de47fb3218699f4e230a4b2169d4
Atlenexe <hugo.mandou@gmail.com>
2023-10-19 10:07:49 +0200

    Asynchrone

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-import {sayHello} from './sayHello.js';
-import {readFileSync} from 'fs';
+import { sayHello } from './sayHello.js';
+import { readFileSync, readFile, writeFile } from 'fs';
 
 sayHello('Lolita');
 
 const file = readFileSync('./package.json');
 
-console.log(JSON.parse(file.toString()).version);
+console.log(JSON.parse(file.toString()).version, ' : Sync');
+
+readFile('./package.json', (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        const version = JSON.parse(data.toString()).version;
+        console.log(version, ' : Async');
+
+        writeFile('./version', version, (err, data) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log('File written');
+            }
+        });
+    }
+});
+
+console.log('Fin du programme');
