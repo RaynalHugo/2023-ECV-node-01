58158ace6affa612022ce3a2f207a48bc68dbfa3
Atlenexe <hugo.mandou@gmail.com>
2023-10-19 09:52:33 +0200

    fs

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import {sayHello} from './sayHello.js';
+import {readFileSync} from 'fs';
+
 sayHello('Lolita');
+
+const file = readFileSync('./package.json');
+
+console.log(JSON.parse(file.toString()).version);
